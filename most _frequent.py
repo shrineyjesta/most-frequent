eade0a2434d50f2f5c883dc3ce66711d9068d521
shrineyjesta <97728736+shrineyjesta@users.noreply.github.com>
2022-01-26 11:06:27 +0530

    Add files via upload

---
+++ most _frequent.py
@@ -0,0 +1,20 @@
+def most_fequent():
+    a=input("please enter string")
+    print(a)
+    thisdict={"i":4,
+          "s":4,
+          "p":2,
+          "m":1
+          }
+    print(thisdict)
+    
+    
+    
+
+most_fequent()
+    
+    
+    
+
+
+
